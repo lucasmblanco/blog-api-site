3d73e63910ea1c7fdd0990c0758baf9956aa6cee
Lucas <lucasmatiasblanco@outlook.com>
2023-09-08 22:00:22 -0300

    Added column, card and post components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Playfair Display Variable', ...defaultTheme.fontFamily.sans],
+				georgia: ['Noto Serif Georgian Variable', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				'black-brown': 'hsl(300, 3%, 12%)',
+				ivory: 'hsl(60, 100%, 97%)'
+			}, 
+			backgroundImage: {
+				'binding': "url('/src/assets/binding-dark.png')"
+			},
+			spacing: {
+				'base-width': "min(100% - 3rem, 65ch)"
+			}
+		},
 	},
 	plugins: [],
 }
